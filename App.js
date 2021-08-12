3dd32ca6f9a61744a13329c46c4aff861c1b040c
Lam-Richard <richardlam2024@gmail.com>
2021-08-11 21:39:32 -0500

    created stock and dashboard screens

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput } from 'react-native';
+import { DashboardScreen } from './screens/DashboardScreen';
+import { StockScreen } from './screens/StockScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const capitalOne = {
+  company: 'Capital One',
+  ticker: 'COF',
+  lastPrice: 174.55,
+  lastChange: 3.91,
+  percentChange: 2.29,
+  upDown: '+',
+  shares: 100,
+  boughtPrice: 8.31,
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+export default function App () {
+  return <StockScreen data={capitalOne}></StockScreen>
+}
+
---
+++ screens/DashboardScreen.js
@@ -0,0 +1,130 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput } from 'react-native';
+
+export const DashboardScreen = () => {
+
+    const capitalOne = {
+      company: 'Capital One',
+      ticker: 'COF',
+      lastPrice: 174.55,
+      lastChange: 3.91,
+      percentChange: 2.29,
+      upDown: '+',
+      shares: 100,
+      boughtPrice: 8.31,
+    }
+  
+    const Ticker = ({data}) => {
+      const backgroundColor = data.upDown == "+" ? 'aquamarine' : 'red'
+      return (
+        <TouchableOpacity style={styles.ticker} onPress={() => {console.log('Pressed')}}>
+          <Text>{data.company} ({data.ticker}):</Text>
+          <View>
+            <Text style={{fontWeight: 'bold'}}>{data.lastPrice}</Text>
+            <Text style={{backgroundColor: backgroundColor}} >{data.upDown}{data.lastChange}</Text>
+          </View>
+        </TouchableOpacity>
+      )
+    }
+  
+    const SearchBar = () => {
+      return (
+        <TextInput placeholder={'Search for companies, articles, or symbols...'} style={styles.search}></TextInput>
+      )
+    }
+  
+    return (
+      <View style={styles.container}>
+        <View style={styles.dashboard}>
+          <Text style={{textAlign: 'center', marginVertical: '4%'}}> My Stuff</Text>
+          <ScrollView>
+            <Ticker data={capitalOne}></Ticker>
+            <Ticker data={capitalOne}></Ticker>
+            <Ticker data={capitalOne}></Ticker>
+            <Ticker data={capitalOne}></Ticker>
+            <Ticker data={capitalOne}></Ticker>
+            <Ticker data={capitalOne}></Ticker>
+            <Ticker data={capitalOne}></Ticker>
+            <Ticker data={capitalOne}></Ticker>
+            <Ticker data={capitalOne}></Ticker>
+            <Ticker data={capitalOne}></Ticker>
+          </ScrollView>
+         
+        </View>
+        <View style={styles.order}>
+          <SearchBar/>
+        </View>
+      </View>
+    );
+  }
+  
+  
+  
+  const styles = StyleSheet.create({
+  
+  
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      paddingTop: '20%',
+      borderStyle: 'solid',
+      borderWidth: 1,
+      borderColor: 'black',
+  
+    },
+    dashboard: {
+      height: '40%',
+      width: '96%',
+      // borderStyle: 'solid',
+      // borderWidth: 1,
+      // borderColor: 'black',
+      marginBottom: '5%'
+    },
+    order: {
+      height: '40%',
+      width: '96%',
+      alignItems: 'center',
+      borderStyle: 'solid',
+      borderWidth: 1,
+      borderColor: 'black',
+      marginBottom: '5%'
+    },
+    end_day: {
+      height: '10%',
+      width: '96%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderStyle: 'solid',
+      borderWidth: 1,
+      borderColor: 'black',
+      marginBottom: '5%'
+    },
+    ticker: {
+      width: '98%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: '1%',
+      marginHorizontal: '1%',
+      borderStyle: 'solid',
+      borderWidth: 1,
+      borderColor: 'black',
+      borderRadius: 10,
+      marginBottom: 5,
+    },
+    search: {
+      width: '95%',
+      paddingHorizontal: '2%',
+      marginTop: '5%',
+      height: '15%',
+      borderStyle: 'solid',
+      borderColor: 'black',
+      borderWidth: 1,
+      borderRadius: 20
+    }
+  
+  
+  });
