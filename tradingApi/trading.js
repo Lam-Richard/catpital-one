ead762105971035850eb6f069d47d95a5cac9225
Sabrina Li <49539018+sl416@users.noreply.github.com>
2021-08-12 11:39:13 -0400

    alpaca api

---
+++ tradingApi/trading.js
@@ -0,0 +1,12 @@
+export const trading = () => {
+    const Alpaca = require('@alpacahq/alpaca-trade-api')
+  
+    const alpaca = new Alpaca({
+      keyId: 'PKRTDHD80018SUDO4TST',
+      secretKey: 'rgCzkzaXjpvUAbrmyGQCr19XpycPgUwY0h42v1gN',
+      paper: true,
+      usePolygon: false
+    })
+    
+  };
+  
