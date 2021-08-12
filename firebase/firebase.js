18853e93c4fec8b3e08ce53c8633148e63e50cbb
Lam-Richard <richardlam2024@gmail.com>
2021-08-11 23:27:11 -0500

    env debug

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,15 +2,24 @@ import firebase from "firebase";
 import "firebase/auth";
 import "firebase/storage";
 
-import {
-    FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID
-} from "@env";
+
+// import {
+//     FIREBASE_API_KEY,
+//     FIREBASE_AUTH_DOMAIN,
+//     FIREBASE_PROJECT_ID,
+//     FIREBASE_STORAGE_BUCKET,
+//     FIREBASE_MESSAGING_SENDER_ID,
+//     FIREBASE_APP_ID,
+//     FIREBASE_MEASUREMENT_ID
+// } from "@env";
+
+const FIREBASE_API_KEY='AIzaSyDJ_HyA4XkDLtVTACOEX4r_ttMeCpDlJ5w'
+const FIREBASE_AUTH_DOMAIN='cat-pitaloneses.firebaseapp.com'
+const FIREBASE_PROJECT_ID='cat-pitaloneses'
+const FIREBASE_STORAGE_BUCKET='cat-pitaloneses.appspot.com'
+const FIREBASE_MESSAGING_SENDER_ID='673048356654'
+const FIREBASE_APP_ID='1:673048356654:web:fb944092cb9fe7064cbcbf'
+const FIREBASE_MEASUREMENT_ID='G-28W7X9C8N6'
 
 
 const firebaseConfig = {
